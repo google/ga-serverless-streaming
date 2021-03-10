52b2104a8fee7f899bdd0debdaf550b323f544e4
Ben Kuehn <bkuehn@google.com>
2021-03-10 17:18:06 -0500

    [dataflow] - GA Stream
    Change-Id: I75fa71e4676f081b39c86a5fe34a1cbc2b6e2264

---
+++ ga_stream_beam/setup.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Configures dependencies and modules."""
+
+import setuptools
+
+setuptools.setup(
+    name='ga-stream-beam',
+    version='1',
+    install_requires=['ua-parser'],
+    packages=setuptools.find_packages(),
+ )
